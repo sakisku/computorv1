60b5d92e46bce7c62c7f4c4d69bb4c5cfc45062c
Saki <sakisku@users.noreply.github.com>
2026-02-05 18:14:36 +0100

    feat: main

---
+++ computor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import sys
+import signal
+
+def main():
+
+    if len(sys.argv) < 2:
+        try:
+            signal.signal(signal.SIGINT,
+                        lambda *_: (print("Interrupted by user"),
+                                    exit(1)))
+            equation = input("")
+            # resolver(equation)
+            print(equation)
+        except Exception as e:
+            print(f"Error: {e}")
+            exit(1)
+    else:
+        equation = sys.argv[1]
+        # resolver(equation)
+        print(equation)
+
+if __name__ == "__main__":
+    main()
