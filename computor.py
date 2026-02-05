e2fc42d266dba559377c7de103317741741577af
Saki <sakisku@users.noreply.github.com>
2026-02-05 19:00:23 +0100

    chore: functions added

--- computor.py
+++ computor.py
@@ -2,6 +2,32 @@
 import sys
 import signal
 
+def discriminant() -> int:
+    return 0 #or -1 or 1
+
+def reducer() -> str:
+    return "reduced"
+def polynomial_degree() -> str:
+    return "1"
+def resolver() -> str:
+
+    # if polynomial() == "0":
+    #     # do something
+    # elif polynomial() == "1":
+    #     # do something else
+    # elif polynomial() == "2":
+    #     # do something else
+    # else:
+    #     # no
+    return "solution"
+
+def display():
+
+    print(f"Reduced form: {reducer()}")
+    print(f"Polynomial degree: {polynomial_degree()}")
+    print(f"{resolver()}")
+
+
 def main():
 
     if len(sys.argv) < 2:
@@ -9,9 +35,9 @@ def main():
             signal.signal(signal.SIGINT,
                         lambda *_: (print("Interrupted by user"),
                                     exit(1)))
-            equation = input("")
-            # resolver(equation)
-            print(equation)
+            equation = input("> ")
+            # get_data(equation)
+            display()
         except Exception as e:
             print(f"Error: {e}")
             exit(1)
@@ -20,8 +46,8 @@ def main():
         exit(1)
     else:
         equation = sys.argv[1]
-        # resolver(equation)
-        print(equation)
+        # get_data(equation)
+        display()
 
 if __name__ == "__main__":
     main()
