4e3a3434c8f042b199eb261a9cb84768ce72ddbc
Saki <sakisku@users.noreply.github.com>
2026-02-05 19:49:30 +0100

    feat: polynomial_degree done

--- computor.py
+++ computor.py
@@ -1,42 +1,51 @@
 #!/usr/bin/env python3
 import sys
 import signal
+import re
+
+equation = ""
 
 def discriminant() -> int:
     return 0 #or -1 or 1
 
 def reducer() -> str:
     return "reduced"
-def polynomial_degree() -> str:
-    return "1"
-def resolver() -> str:
 
-    # if polynomial() == "0":
-    #     # do something
-    # elif polynomial() == "1":
-    #     # do something else
-    # elif polynomial() == "2":
-    #     # do something else
-    # else:
-    #     # no
-    return "solution"
+def polynomial_degree() -> int:
+    global equation
+    exponents = re.findall(r"X\^(\d+)", equation)
+    if not exponents:
+        return 0
+    return int(max(exponents))
+
+def resolver() -> str:
+    degree = polynomial_degree()
+    if degree == 0:
+        return "constant function"
+    elif degree == 1:
+        return "linear function"
+    elif degree == 2:
+        return "quadratic function"
 
 def display():
-
+    degree = polynomial_degree()
     print(f"Reduced form: {reducer()}")
-    print(f"Polynomial degree: {polynomial_degree()}")
+    print(f"Polynomial degree: {degree}")
+    if degree > 2:
+        print("The polynomial degree is strictly greater than 2, I can't solve.")
+        exit(1)
     print(f"{resolver()}")
 
 
 def main():
-
+    global equation
     if len(sys.argv) < 2:
         try:
             signal.signal(signal.SIGINT,
                         lambda *_: (print("Interrupted by user"),
                                     exit(1)))
-            equation = input("> ")
-            # get_data(equation)
+            data = input("> ")
+            equation = data
             display()
         except Exception as e:
             print(f"Error: {e}")
@@ -46,7 +55,6 @@ def main():
         exit(1)
     else:
         equation = sys.argv[1]
-        # get_data(equation)
         display()
 
 if __name__ == "__main__":
